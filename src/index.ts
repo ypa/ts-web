9ff07c0b1781de9d5acb03ac4227ac884af28c80
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-22 10:41:50 -0800

    Test out User class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hi there!");
+import { User } from './models/User';
+
+const user = new User({name: 'myname', age: 20});
+
+user.set({ name: "newname", age: 999 });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
