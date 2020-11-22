fee01c286602eb38dd87c1495b40c19a39a8e4eb
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-22 11:07:33 -0800

    Add event call back mechanism on User

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,9 +3,12 @@ interface UserProps {
   age?: number;
 }
 
-export class User {
+type Callback = () => void;
 
-  constructor(private data: UserProps) {}
+export class User {
+  events: { [key: string]: Callback[] } = {};
+
+  constructor(private data: UserProps) { }
 
   get(propName: string): (number | string) {
     return this.data[propName];
@@ -13,5 +16,23 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach(callback => {
+      callback();
+    });
   }
 }
