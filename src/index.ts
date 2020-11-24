77e17f98b847e481fdbe8c3c2d68b0d8e58bc629
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-24 07:26:53 -0800

    Test that eventing still works

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ import { User } from './models/User';
 
 const user = new User({ name: 'new record', age: 0 });
 
-user.save();
+user.events.on('change', () => {
+  console.log('Changed!');
+})
+
+user.events.trigger('change');
