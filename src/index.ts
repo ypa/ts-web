8886563464dcf6ff223bd25b813d44f4b69eb7a4
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-24 07:01:05 -0800

    Test out the save method

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-import axios from 'axios';
+import { User } from './models/User';
 
-axios.get('http://localhost:3000/users/1');
+const user = new User({ name: 'new record', age: 0 });
+
+user.save();
