dd82b88e09ab6826f2e847e1d5602f3ea50967a2
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-26 10:36:13 -0800

    Extract Attributes generics class out of User

---
+++ src/models/Attributes.ts
@@ -0,0 +1,12 @@
+export class Attributes<T> {
+  constructor(private data: T) {}
+
+  get(propName: string): (number | string | boolean) {
+    return this.data[propName];
+  }
+
+  set(update: T): void {
+    Object.assign(this.data, update);
+  }
+}
+
