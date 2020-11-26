67a75e40ede941df0ad88b027ed70106ad3dac7c
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-26 11:05:22 -0800

    Revert "Test out the types for Attributes getter method"
    This reverts commit 1459f6a36b123dc3213b9a1ac6fdb890e563fd0a.

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,7 +1,5 @@
-import { UserProps } from './User';
-
 export class Attributes<T> {
-  constructor(private data: T) { }
+  constructor(private data: T) {}
 
   get<K extends keyof T>(key: K): T[K] {
     return this.data[key];
@@ -12,13 +10,3 @@ export class Attributes<T> {
   }
 }
 
-
-const attrs = new Attributes<UserProps>({
-  id: 5,
-  age: 20,
-  name: 'afdasf'
-});
-
-const name = attrs.get('name');
-const age = attrs.get('age');
-const id = attrs.get('id');
