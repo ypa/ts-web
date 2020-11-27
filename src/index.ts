40fb9336ebf4c1a105d7bf0b89ac4058502ce12f
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-27 09:26:20 -0800

    Test out user.get and seeing error which is caused by JS 'this' context issue Add code that sort of expalins how JS funcion context 'this' works

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,24 @@ import { User } from './models/User';
 
 const user = new User({ name: 'new record', age: 0 });
 
-user.events.on('change', () => {
-  console.log('Changed!');
-})
+// console.log(user.get('name'));
 
-user.events.trigger('change');
+
+// reminder on how 'this' workds in Javascript
+
+const colors = {
+  color: 'red',
+  printColor() {
+    // rule of thumb for 'this' is it's always referring to the left of function
+    console.log(this.color);
+  }
+}
+
+// Here 'this' is the object colors so it prints out 'red' just fine
+colors.printColor();
+
+// if we destructure printColor out to another variable
+const printColor = colors.printColor;
+// And call it, now 'this' is undefined
+// and the function throws exception TypeError: Cannot read property 'color' of undefined
+printColor();
