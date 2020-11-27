d2df109191b34a474615a54a447f362b72f1f9d2
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-27 09:45:10 -0800

    Remove JS context issue example code and test out new user delegation methods

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,10 @@ import { User } from './models/User';
 
 const user = new User({ name: 'new record', age: 0 });
 
-// console.log(user.get('name'));
+console.log(user.get('name'));
 
+user.on('change', () => {
+  console.log('User was changed');
+});
 
-// reminder on how 'this' workds in Javascript
-
-const colors = {
-  color: 'red',
-  printColor() {
-    // rule of thumb for 'this' is it's always referring to the left of function
-    console.log(this.color);
-  }
-}
-
-// Here 'this' is the object colors so it prints out 'red' just fine
-colors.printColor();
-
-// if we destructure printColor out to another variable
-const printColor = colors.printColor;
-// And call it, now 'this' is undefined
-// and the function throws exception TypeError: Cannot read property 'color' of undefined
-printColor();
+user.trigger('change');
