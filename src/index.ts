25229d6a208c1e5ad6ff66e7bf3949fbe3bf14b1
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-30 07:52:28 -0800

    Test out the new Collection with generics

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+import { User, UserProps } from './models/User';
 import { Collection } from './models/Collection';
 
 
-const collection = new Collection('http://localhost:3000/users');
+const collection = new Collection<User, UserProps>(
+  'http://localhost:3000/users',
+  User.buildUser
+);
 
 collection.on('change', () => {
   console.log(collection);
