bf0808ae1b93293b0a861020a4fa82372e211505
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-01 07:35:50 -0800

    Test out another event moseover on H1 element

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -3,8 +3,13 @@ export class UserForm {
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      'click:button': this.onButtonClick
+      'click:button': this.onButtonClick,
+      'mouseover:h1': this.onHeaderHover
     };
+  }
+
+  onHeaderHover(): void {
+    console.log('H1 was hover over');
   }
 
   onButtonClick(): void {
