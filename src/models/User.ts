55bc74141cdffacf607cfe4789bb985bc629c15e
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-01 06:47:49 -0800

    Add static method buildUserCollection to User class

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,6 +2,7 @@ import { Model } from './Model';
 import { Attributes } from './Attributes';
 import { ApiSync } from './ApiSync';
 import { Eventing } from './Eventing';
+import { Collection } from './Collection';
 
 export interface UserProps {
   id?: number;
@@ -18,5 +19,9 @@ export class User extends Model<UserProps> {
       new Eventing(),
       new ApiSync<UserProps>(rootUrl)
     );
+  }
+
+  static buildUserCollection(): Collection<User, UserProps> {
+    return new Collection<User, UserProps>(rootUrl, this.buildUser);
   }
 }
