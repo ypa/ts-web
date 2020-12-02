857b668f2baef4fe5386aa601fb490baccf972a1
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-02 07:07:31 -0800

    Test out UserForm with user model

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { UserForm } from './views/UserForm';
+import { User } from './models/User';
 
+const user = User.buildUser({ name: 'NAME', age: 20 });
 const userForm = new UserForm(
-  document.getElementById('root')
+  document.getElementById('root'),
+  user
 );
 
 userForm.render();
