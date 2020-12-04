c4ecfa5f440262d9f7dc355a21f6debbd700d941
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-04 07:23:53 -0800

    Use Model as generics of view, now generics need to take 2 type parameters to satisfiy Model type as well which itself is a generics type

--- src/views/View.ts
+++ src/views/View.ts
@@ -1,8 +1,6 @@
-interface ModelForView {
-  on(eventName: string, callback: () => void): void;
-}
+import { Model } from '../models/Model';
 
-export abstract class View<T extends ModelForView> {
+export abstract class View<T extends Model<K>, K> {
   constructor(public parent: Element, public model: T) {
     this.bindModel();
   }
